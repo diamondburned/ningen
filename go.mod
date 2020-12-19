15610044db9a270e4d546964c2d389a7299c07b8
diamondburned <datutbrus@gmail.com>
2020-12-18 23:03:01 -0800

    wait for ReadySupplemental in Open

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/diamondburned/ningen/v2
 go 1.14
 
 require (
-	github.com/diamondburned/arikawa/v2 v2.0.0-20201208195035-9911a3d6627a
+	github.com/diamondburned/arikawa/v2 v2.0.0-20201219065126-f11edb7260b2
 	github.com/pkg/errors v0.9.1
 	github.com/twmb/murmur3 v1.1.3
 	github.com/yuin/goldmark v1.1.30
--- ningen.go
+++ ningen.go
@@ -4,7 +4,6 @@ package ningen
 
 import (
 	"sort"
-	"sync"
 
 	"github.com/diamondburned/arikawa/v2/discord"
 	"github.com/diamondburned/arikawa/v2/gateway"
@@ -34,7 +33,6 @@ type State struct {
 	// handler that users can call
 
 	initd chan struct{} // nil after Open().
-	initm sync.Mutex
 
 	// nil before Open().
 	NoteState         *note.State
@@ -48,6 +46,7 @@ type State struct {
 // FromState wraps a normal state.
 func FromState(s *state.State) (*State, error) {
 	state := &State{
+		initd:      make(chan struct{}, 1),
 		State:      s,
 		Handler:    handler.New(),
 		prehandler: handler.New(),
@@ -76,48 +75,45 @@ func FromState(s *state.State) (*State, error) {
 		state.prehandler.Call(v)
 
 		switch v.(type) {
-		case *gateway.ReadyEvent, *gateway.ResumedEvent:
+		// Might be better to trigger this on a ReadySupplemental event, as
+		// that's when things are truly done?
+		case *gateway.ReadySupplementalEvent, *gateway.ResumedEvent:
 			state.Handler.Call(&Connected{v})
+		}
+
+		// Only unblock if we have a ReadySupplemental to ensure that we have
+		// everything in the state.
+		if _, ok := v.(*gateway.ReadySupplementalEvent); ok {
+			// Send to channel that unblocks Open() so applications don't access nil
+			// states and avoid data race.
+			select {
+			case state.initd <- struct{}{}:
+				// Since this channel is one-buffered, we can do this.
+			default:
+			}
 		}
 
 		// Call the external handler after we're done. This handler is
 		// asynchronuos, or at least it should be.
 		state.Handler.Call(v)
-
-		state.initm.Lock()
-		initd := state.initd
-		state.initm.Unlock()
-
-		// Send to channel that unblocks Open() so applications don't access nil
-		// states and avoid data race.
-		if initd != nil {
-			initd <- struct{}{}
-		}
 	})
 
 	return state, nil
 }
 
 func (s *State) Open() error {
-	// Make the channel so the ready handler can use it. This channel is
-	// 1-buffered in case the handler is faster than us.
-	s.initm.Lock()
-	s.initd = make(chan struct{}, 1)
-	s.initm.Unlock()
+	// Ensure the channel is free.
+	select {
+	case <-s.initd:
+	default:
+	}
 
 	if err := s.State.Open(); err != nil {
 		return err
 	}
 
-	s.initm.Lock()
-	initd := s.initd
-	s.initm.Unlock()
-
-	<-initd
-
-	s.initm.Lock()
-	s.initd = nil // so future Ready events will never use this ch
-	s.initm.Unlock()
+	// Wait until ReadySupplementalEvent.
+	<-s.initd
 
 	return nil
 }
